ae74c80a96ea401ec62f94822bd8c38fc48f8804
Thorsten Kunz <thorsten.kunz@adesso.de>
2025-08-04 09:08:10 +0200

    Add ServiceCollectionExtensions for DI registration

---
+++ src/CryptoExchangeTask.Business/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using CryptoExchangeTask.Business.ExecutionPlan;
+using CryptoExchangeTask.Business.ExecutionPlan.Calculators;
+using CryptoExchangeTask.Business.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CryptoExchangeTask.Business.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddBusinessServices(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddScoped<IExecutionPlanService, ExecutionPlanService>();
+        serviceCollection.AddScoped<IExchangeRepository, JsonExchangeRepository>();
+        serviceCollection.AddScoped<IExchangeJsonSerializer, ExchangeJsonSerializer>();
+
+        serviceCollection.AddScoped<IExecutionPlanCalculatorFactory, ExecutionPlanCalculatorFactory>();
+        serviceCollection.AddScoped<BuyerExecutionPlanCalculator>();
+        serviceCollection.AddScoped<SellerExecutionPlanCalculator>();
+
+        return serviceCollection;
+    }
+}
